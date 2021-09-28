8f6d42aa1edbfacf51abfc7415bee1ed3abe5969
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2021-09-27 22:07:45 -0300

    Initial commit

---
+++ aula6/aula6.py
@@ -0,0 +1,11 @@
+# Variáveis
+
+nome = 'Marco'
+print(nome)
+idade = 55
+altura = 1.70
+emaior = idade > 18
+print('Idade', idade)
+print('Altura', altura)
+print('É maior', emaior)
+
---
+++ teste1.py
@@ -0,0 +1,17 @@
+a = int(input('Digite um número para multiplar: '))
+for b in range(1, 11):
+    t = b * a
+    print(t, end=' ')
+"""
+for c in range(1, 21):
+    if c % 2 == 0:
+        print('-=' * 5)
+        print(f'{c} é par')
+    elif c % 2 == 1:
+        print('-=' * 5)
+        print(f'{c} é impar') """
+
+
+
+
+

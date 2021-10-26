de8ed1ad95df1d06ef29209263e89c5c4a483ede
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2021-10-25 21:01:32 -0300

    map e filter

---
+++ aula39/mapeamento.py
@@ -0,0 +1,9 @@
+from dados import produtos, pessoa, lista
+
+nova_lista = map(lambda x: x * 2, lista)
+print(lista)
+print(list(nova_lista))
+print('*' * 100)
+nova_lista2 = [x * 2 for x in lista]
+print(nova_lista2)
+
---
+++ aula40/filter.py
@@ -0,0 +1,10 @@
+from dados import produtos, pessoas, lista
+
+nova_lista = filter(lambda x: x > 5, lista)
+print(list(nova_lista))
+
+print('*' * 100)
+
+nova_lista2 = [x for x in lista if x > 5]
+print(nova_lista2)
+

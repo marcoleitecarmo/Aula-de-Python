ac2f33a4e2e666e4d3693b699516c9b7dcfb8601
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2021-11-10 21:03:37 -0300

    getters e setters

---
+++ aula57/gettersSetters.py
@@ -0,0 +1,20 @@
+# setter = configurando valor (=)
+# setter função de configura um valor
+
+# getter = obter um valor (.)
+
+class Pessoa:
+    def __init__(self, nome):
+        self._nome = nome
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+
+p1 = Pessoa('Otavio')
+print(p1.nome)

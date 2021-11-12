8e871ce12537601060a0ba964abe17c61822c2ea
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2021-11-11 21:40:48 -0300

    agregação, associação, herança simples e multipla, sobreposição

---
+++ aula63/herancaMultiplaPoo.py
@@ -0,0 +1,21 @@
+class A:
+    def falar(self):
+        print('Falando.....em classe A')
+
+
+class B(A):
+    def falar(self):
+        print('Falando...Classe B')
+
+
+class C(A):
+    def falar(self):
+        print('Falando...Classe C')
+
+
+class D(C, B):
+    pass
+
+
+d = D()
+d.falar()
